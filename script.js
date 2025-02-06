9309eb9342ed924cf884e8c2e312be94dd1e8358
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-02-06 13:15:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  
+	let charCount = {};
+
+   for (let i = 0; i < str.length; i++) {
+		charCount[str[i]] = (charCount[str[i]] || 0) + 1;
+   }
+	for (let i = 0; i < str.length; i++) {
+		 if (charCount[str[i]] === 1) {
+		  return str[i];
+		 }
+	}
+	return null; 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
